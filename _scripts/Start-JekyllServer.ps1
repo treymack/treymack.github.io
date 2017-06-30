ceb216a1eaf2658a6e29c8a2fd70fcad23e4b1a1
Trey Mack <tmack10@gmail.com>
2017-06-30 10:58:05 -0400

    Get-ADObject and add switch for draft preview

--- _scripts/Start-JekyllServer.ps1
+++ _scripts/Start-JekyllServer.ps1
@@ -1,1 +1,11 @@
-vagrant ssh -c "cd /vagrant && jekyll serve --watch --host 0.0.0.0 --force_polling --drafts"
+param (
+    [Switch]
+    $IncludeDrafts
+)
+if ($IncludeDrafts) {
+    $drafts = "--drafts"
+} else {
+    $drafts = ""
+}
+
+vagrant ssh -c "cd /vagrant && jekyll serve --watch --host 0.0.0.0 --force_polling $drafts"
