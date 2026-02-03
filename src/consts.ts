62cd80414d586008d2ecd8e19aced4fd6bd865a8
Trey Mack <tmack10@gmail.com>
2026-02-02 19:43:37 -0500

    Add Giscus comments to blog posts
    - Create CommentsSection component with GitHub Discussions integration
    - Integrate component into PostLayout for all blog posts
    - Update Giscus config to use Announcements category
    - Comments support threading, moderation via GitHub
    - Requires GitHub authentication to comment

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,33 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Trey Mack';
-export const SITE_DESCRIPTION = 'I am a full stack web developer specializing in reactive web applications and integration. Feel free to contact me on Twitter or via email.';
-export const SITE_EMAIL = 'tmack10@gmail.com';
-export const TWITTER_USERNAME = 'trey_mack';
-export const GITHUB_USERNAME = 'treymack';
-export const GA_ID = 'UA-96898181-1';
+export const SITE_TITLE = "Trey Mack";
+export const SITE_DESCRIPTION =
+  "I am a full stack web developer specializing in reactive web applications and integration. Feel free to contact me on Twitter or via email.";
+export const SITE_EMAIL = "tmack10@gmail.com";
+export const TWITTER_USERNAME = "trey_mack";
+export const GITHUB_USERNAME = "treymack";
+export const GA_ID = "UA-96898181-1";
+
+export const GISCUS_SCRIPT = `
+<script src="https://giscus.app/client.js"
+        data-repo="treymack/treymack.github.io"
+        data-repo-id="MDEwOlJlcG9zaXRvcnkzODkwNDU3Ng=="
+        data-category="Announcements"
+        data-category-id="DIC_kwDOAlGjAM4C1ypE"
+        data-mapping="pathname"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="bottom"
+        data-theme="preferred_color_scheme"
+        data-lang="en"
+        data-loading="lazy"
+        crossorigin="anonymous"
+        async>
+</script>`;
+// Giscus comments configuration
+export const GISCUS_REPO = "treymack/treymack.github.io";
+export const GISCUS_REPO_ID = "MDEwOlJlcG9zaXRvcnkzODkwNDU3Ng=="; // From giscus.app
+export const GISCUS_CATEGORY = "Announcements";
+export const GISCUS_CATEGORY_ID = "DIC_kwDOAlGjAM4C1ypE"; // From giscus.app
